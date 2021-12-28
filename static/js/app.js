53389b15844216122561837ad6d403ccd8b234bf
Paula Thompson <paula.b.thompson@outlook.com>
2021-12-28 11:52:05 -0800

    final code for app.js

--- static/js/app.js
+++ static/js/app.js
@@ -22,3 +22,26 @@ function buildTable(data) {
         );
     });
 }
+
+// function for creating filter
+function handleClick() {
+    // grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    //check to see if a date was entered and filter data using that date
+    if (date) {
+        //apply filter to the table data to only keep rows where the datetime value matches the fitler value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // rebuild the table using the filtered data
+    // if no date entered then default if original tableData
+    buildTable(filteredData)
+}
+
+// attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+//build table when page loads
+buildTable(tableData);
